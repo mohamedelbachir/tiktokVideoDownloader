d9f019a36dfd51f0095fa2958fa86cfa03aa5ac3
mohamedelbachir <76158313+mohamedelbachir@users.noreply.github.com>
2023-02-04 03:13:11 +0100

    initial upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const searchElement=document.querySelector("#idsearch");
+const from=document.querySelector("#from-search");
+
+from.addEventListener('onSubmit',(e)=>{
+    console.log("hey");
+})
+async function searchVideo(query){
+    
+}
